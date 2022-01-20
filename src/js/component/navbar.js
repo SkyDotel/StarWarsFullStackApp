f0a6456b6b9fba06355b14cdd530b2ff295be5f0
Keenan <kkan68@gmail.com>
2022-01-20 02:15:53 +0000

    HTML and CSS being built for now

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,17 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<img height="70" width="125" src="https://www.citypng.com/public/uploads/preview/-51608494584ia2sfbncsd.png"></img>
+				</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="dropdown">
+					<button className="btn btn-primary dropdown-toggle" id="dropdownMenu2" data-toggle="dropdown"aria-expanded="false" data-bs-toggle="dropdown">Favorites</button>
+					<div className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenu2">
+						<button className="dropdown-item" type="button">Action</button>
+						<button className="dropdown-item" type="button">Another action</button>
+						<button className="dropdown-item" type="button">Something else here</button>
+  					</div>
 			</div>
 		</nav>
 	);
